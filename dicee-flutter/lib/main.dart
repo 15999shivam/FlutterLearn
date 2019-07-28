9e8d7068a284b0120075e937966362114b4647f5
Shivam <shivamgarg260@gmail.com>
2019-07-28 20:14:10 +0530

    Completed Dicee App

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 5;
+  int rightDiceNo = 5;
+  void changeDice() {
+    setState(() {
+      rightDiceNo = Random().nextInt(6) + 1;
+      leftDiceNo = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: FlatButton(
+                padding: EdgeInsets.all(0),
+                child: Image.asset('images/dice$leftDiceNo.png'),
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: FlatButton(
+                padding: EdgeInsets.all(0),
+                child: Image.asset('images/dice$rightDiceNo.png'),
+                onPressed: () {
+                  changeDice();
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
