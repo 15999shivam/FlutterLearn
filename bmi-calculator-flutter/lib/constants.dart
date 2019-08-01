e09ab8f69771079b9b566f8c40c026011b5c04d8
Shivam <shivamgarg260@gmail.com>
2019-08-01 21:54:04 +0530

    Created Seprate Constants for app

---
+++ bmi-calculator-flutter/lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const kIconSize = 80.0;
+const kSizeBoxHeight = 15.0;
+const kTextFontSize = 18.0;
+const kTextStyle = TextStyle(
+  fontSize: kTextFontSize,
+  color: Color(0xFF8D8E98),
+);
+const kHeightNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xFF1D1F33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomCardColor = Color(0xFFEA1556);
+const kMaxSliderLimit = 220.0;
+const kMinSliderLimit = 120.0;
+const kSliderActiveColor = Color(0xFFEB1555);
+const kSliderInactiveColor = Color(0xFF8D8E98);
